fbcf481a751ef5f2d242f34b701e62ad533b859c
BeatrizAbreu <43205130+BeatrizAbreu@users.noreply.github.com>
2019-07-08 17:20:49 +0100

    Add files via upload

---
+++ stringOfDataRAW.py
@@ -0,0 +1,29 @@
+def returnStringOfDataRAW (dataRaw) :
+    float width, height, xCord, yCord;
+    for line in dataRaw:
+        for word in line.split():
+           if (word = 'WIDTH') #width
+           {
+               word += 2
+               width = word
+               while(word != '0')
+               {
+                   word ++
+                   width * 10
+                   width + word
+                   }
+               }
+           else if (word = 'HEIGHT') #height
+           {
+               word += 2
+               width = word
+               while(word != '0')
+               {
+                   word ++
+                   width * 10
+                   width + word
+                   }
+               }
+    for(int i = 0; i < nLines; i++)
+        
+    print(dataRaw)
